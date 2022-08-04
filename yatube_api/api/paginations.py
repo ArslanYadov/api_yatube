06dfb18dc57a587bd07b9e4cf9032d51caa49f00
ArslanYadov <arsyy90@gmail.com>
2022-08-04 19:39:37 +0300

    move number from pagionations.py to setting in const var

--- yatube_api/api/paginations.py
+++ yatube_api/api/paginations.py
@@ -1,5 +1,7 @@
 from rest_framework.pagination import LimitOffsetPagination
 
+from yatube_api.settings import AMOUNT_OBJECTS_PER_PAGE
+
 
 class PostPagination(LimitOffsetPagination):
-    limit_query_description = 5
+    limit_query_description = AMOUNT_OBJECTS_PER_PAGE
